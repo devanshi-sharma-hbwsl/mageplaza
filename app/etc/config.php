807b088457820e25ec919cf2ac8b18de26880387
devanshi sharma <devanshi.sharma@hbwsl.com>
2025-02-20 10:51:26 +0530

    User_Story_7: To Create Hello World Module

--- app/etc/config.php
+++ app/etc/config.php
@@ -355,6 +355,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Macademy_Jumpstart' => 1,
+        'Mageplaza_HelloWorld' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeCustomerBalance' => 1,
         'PayPal_BraintreeGiftCardAccount' => 1,
